4554a514b340c57faa3c640222a2fd2f39742a57
nodejstutorial <nodejstutorial@free.fr>
2014-07-13 22:32:57 +0000

    added 02-parallel

---
+++ 01-helloworld/helloworld.js
@@ -0,0 +1,15 @@
+//import module http
+var http = require('http');
+
+//create onRequest function handler
+function onRequest(req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello Node.js\n');
+}
+
+//create the http server and start listening
+var server = http.createServer(onRequest);
+server.listen(3000, '0.0.0.0');
+
+//logging to console
+console.log('Hello running at http://0.0.0.0:3000/');
---
+++ 02-parallel/parallel.js
@@ -0,0 +1,16 @@
+// http://debuggable.com/posts/understanding-node-js:4bd98440-45e4-4a9a-8ef7-0f7ecbdd56cb
+
+var fs = require('fs'), sys = require('sys');
+
+fs.readFile('hello.txt', function(err,data) {
+  sys.puts("hello: "+data);
+});
+
+// unlink doesn't delete the file, do you know why ?
+fs.unlink('letter.txt',function(err){
+  sys.puts('error deleting');
+});
+
+fs.writeFile('letter.txt', 'hello world @' + new Date().getTime(), function() {
+  sys.puts("hello world @" + new Date().getTime());
+});
